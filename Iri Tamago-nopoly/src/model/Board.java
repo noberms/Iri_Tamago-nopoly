2fb4cc544c9c1ca6a7eb783edf8ab5d02d2f53db
Noel Bermudez <noel.chua.bermudez@gmail.com>
2012-11-19 20:55:06 +0800

    Board: Minor adjustment

--- Iri Tamago-nopoly/src/model/Board.java
+++ Iri Tamago-nopoly/src/model/Board.java
@@ -33,8 +33,8 @@ public class Board {
 		this.board[7] = new TileWrapper(219, 653, cc);
 		this.board[8] = new TileWrapper(157, 653, new Estate("Euston Road", "Estate Image", 100, 50, "cyan", 50, 50, new int[]{6, 30, 90, 270, 400, 550}));
 		this.board[9] = new TileWrapper(95, 653, new Estate("Pentonville Road", "Estate Image", 120, 60, "cyan", 50, 50, new int[]{8, 40, 100, 300, 450, 600}));
-		this.board[10] = new TileWrapper(0, 653, new Tile("In Jail", "Tile Image"));
 		
+		this.board[10] = new TileWrapper(0, 653, new Tile("In Jail", "Tile Image"));
 		this.board[11] = new TileWrapper(0, 564, new Estate("Pall Mall", "Estate Image", 140, 70, "magenta", 100, 100, new int[]{10, 50, 150, 450, 625, 750}));
 		this.board[12] = new TileWrapper(0, 503, new Utility("Electric Company", "Utility Image", 150, 75, new int[]{4, 10}));
 		this.board[13] = new TileWrapper(0, 422, new Estate("Whitehall", "Estate Image", 140, 70, "magenta", 100, 100, new int[]{10, 50, 150, 450, 625, 750}));
@@ -44,8 +44,8 @@ public class Board {
 		this.board[17] = new TileWrapper(0, 198, new CommunityChest("Community Chest Image 2", ccCards));
 		this.board[18] = new TileWrapper(0, 137, new Estate("Marlborough Street", "Estate Image", 180, 90, "orange", 100, 100, new int[]{14, 70 , 200, 550, 750, 950}));
 		this.board[19] = new TileWrapper(0, 76, new Estate("Vine Street", "Estate Image", 200, 100, "orange", 100, 100, new int[]{16, 80, 220, 600, 800, 1000}));
-		this.board[20] = new TileWrapper(0, 0, new Tile("Free Parking", "Tile Image"));
 		
+		this.board[20] = new TileWrapper(0, 0, new Tile("Free Parking", "Tile Image"));
 		this.board[21] = new TileWrapper(95, 0, new Estate("Strand", "Estate Image", 220, 110, "red", 150, 150, new int[]{18, 90, 250, 700, 875, 1050}));
 		this.board[22] = new TileWrapper(157, 0, new CommunityChest("Community Chest Image 2", cchCards));
 		this.board[23] = new TileWrapper(219, 0, new Estate("Fleet Street", "Estate Image", 220, 110, "red", 150, 150, new int[]{18, 90, 250, 700, 875, 1050}));
@@ -55,8 +55,8 @@ public class Board {
 		this.board[27] = new TileWrapper(467, 0, new Estate("Coventry Street", "Estate Image", 260, 130, "yellow", 150, 150, new int[]{22, 110, 330, 800, 975, 1150}));
 		this.board[28] = new TileWrapper(529, 0, new Utility("Water Works", "Utility Image", 150, 75, new int[]{4, 10}));
 		this.board[29] = new TileWrapper(591, 0, new Estate("Piccadilly",  "Estate Image",280, 140, "yellow", 150, 150, new int[]{24, 120, 360, 850, 1025, 1200}));
-		this.board[30] = new TileWrapper(653, 0, new Tile("Go to Jail", "Tile Image"));
 		
+		this.board[30] = new TileWrapper(653, 0, new Tile("Go to Jail", "Tile Image"));
 		this.board[31] = new TileWrapper(653, 76, new Estate("Regent Street", "Estate Image", 300, 150, "green", 200, 200, new int[]{26, 130, 390, 900, 1100, 1275}));
 		this.board[32] = new TileWrapper(653, 137, new Estate("Oxford Street", "Estate Image", 300, 150, "green", 200, 200, new int[]{26, 130, 390, 900, 1100, 1275}));
 		this.board[33] = new TileWrapper(653, 198, new CommunityChest("Community Image 3", cchCards));
