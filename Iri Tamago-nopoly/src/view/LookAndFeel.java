c39fc409a9f5233b431464def688f05fbfb70e83
noberms <noel.chua.bermudez@gmail.com>
2012-11-20 12:21:33 +0800

    Enum look and feel values

---
+++ Iri Tamago-nopoly/src/view/LookAndFeel.java
@@ -0,0 +1,16 @@
+package view;
+
+public enum LookAndFeel {
+	METAL("com.sun.java.swing.plaf.metal.MetalLookAndFeel"),
+	NIMBUS("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel"),
+	MOTIF("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+	
+	private final String path;
+	
+	LookAndFeel(String path) {
+		this.path = path;
+	}
+	
+	public String path(){return this.path;}
+	
+}
