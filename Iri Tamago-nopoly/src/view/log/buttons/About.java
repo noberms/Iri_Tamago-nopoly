c3700a4b595f3136ff096a2f7413931c96bde2f1
noberms <noel.chua.bermudez@gmail.com>
2012-11-20 09:52:31 +0800

    Testing

--- Iri Tamago-nopoly/src/view/log/buttons/About.java
+++ Iri Tamago-nopoly/src/view/log/buttons/About.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class About extends JButton {
-	
+	//Test
 	private static final long serialVersionUID = -3451147928579128619L;
 	
 	public About() {
