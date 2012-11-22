051f342254cbe584b281fd479f39fc3795093e0f
Noel Bermudez <noel.chua.bermudez@gmail.com>
2012-11-23 01:56:03 +0800

    Game board background images

--- Iri Tamago-nopoly/src/view/GameBoard.java
+++ Iri Tamago-nopoly/src/view/GameBoard.java
@@ -1,6 +1,13 @@
 package view;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+//import javax.swing.JPanel;
 
 
 public class GameBoard extends JFrame {
@@ -9,7 +16,7 @@ public class GameBoard extends JFrame {
 	
 	public GameBoard() {
 		this.setTitle("Iri Tamagonopoly - Game Board");
-		this.setSize(800, 600);
+		this.setSize(721, 580);
 		this.setLocation(200, 200);
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -18,6 +25,19 @@ public class GameBoard extends JFrame {
 	}
 	
 	private void initComponents() {
+		
+		JLabel backgroundLabel = new JLabel();
+		//backgroundLabel.setIcon(new ImageIcon(getClass().getResource("../images/BoardFrame.jpg")));
+		try {
+			backgroundLabel.setIcon(new ImageIcon(ImageIO.read(new File("images/BoardFrame.jpg"))));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.add(backgroundLabel);
+		
+		//JPanel backgroundPanel = new JPanel();
+		
 		
 	}
 	
