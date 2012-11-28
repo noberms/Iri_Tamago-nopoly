a989f105b25767abbea970a48e8011c307d5ac81
Noel Bermudez <noel.chua.bermudez@gmail.com>
2012-11-28 23:25:35 +0800

    DeedDialog is not modal

--- Iri Tamago-nopoly/src/view/modifiedComponents/DeedDialog.java
+++ Iri Tamago-nopoly/src/view/modifiedComponents/DeedDialog.java
@@ -22,7 +22,7 @@ public class DeedDialog extends JDialog {
 		int height = 445;
 		this.setSize(width, height);
 		this.setResizable(false);
-		//this.setLocation(LocationHelper.getInstance().getScreenCenter(this));
+		this.setModal(true);
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		this.initComponents(deedIndex);
 	}
